edf8d7843224c3ac69dcf20dcb30eac3565231c0
Daniel Mahefasoa Brady <dmbrady01@gmail.com>
2019-04-02 23:40:05 -0400

    Attempt at multiple symbols

--- tick_taker.py
+++ tick_taker.py
@@ -109,7 +109,7 @@ class Position():
 
 
 def run(args):
-    symbol = args.symbol
+    symbol_list = args.symbols
     max_shares = args.quantity
     opts = {}
     if args.key_id:
@@ -120,125 +120,127 @@ def run(args):
         opts['base_url'] = args.base_url
     elif 'key_id' in opts and opts['key_id'].startswith('PK'):
         opts['base_url'] = 'https://paper-api.alpaca.markets'
-    # Create an API object which can be used to submit orders, etc.
-    api = tradeapi.REST(**opts)
 
-    symbol = symbol.upper()
-    quote = Quote()
-    qc = 'Q.%s' % symbol
-    tc = 'T.%s' % symbol
-    position = Position()
+    for symbol in symbol_list:
+        # Create an API object which can be used to submit orders, etc.
+        api = tradeapi.REST(**opts)
 
-    # Establish streaming connection
-    conn = tradeapi.StreamConn(**opts)
+        symbol = symbol.upper()
+        quote = Quote()
+        qc = 'Q.%s' % symbol
+        tc = 'T.%s' % symbol
+        position = Position()
 
-    # Define our message handling
-    @conn.on(r'Q\.' + symbol)
-    async def on_quote(conn, channel, data):
-        # Quote update received
-        quote.update(data)
+        # Establish streaming connection
+        conn = tradeapi.StreamConn(**opts)
 
-    @conn.on(r'T\.' + symbol)
-    async def on_trade(conn, channel, data):
-        if quote.traded:
-            return
-        # We've received a trade and might be ready to follow it
-        if (
-            data.timestamp <= (
-                quote.time + pd.Timedelta(np.timedelta64(50, 'ms'))
-            )
-        ):
-            # The trade came too close to the quote update
-            # and may have been for the previous level
-            return
-        if data.size >= 100:
-            # The trade was large enough to follow, so we check to see if
-            # we're ready to trade. We also check to see that the
-            # bid vs ask quantities (order book imbalance) indicate
-            # a movement in that direction. We also want to be sure that
-            # we're not buying or selling more than we should.
+        # Define our message handling
+        @conn.on(r'Q\.' + symbol)
+        async def on_quote(conn, channel, data):
+            # Quote update received
+            quote.update(data)
+
+        @conn.on(r'T\.' + symbol)
+        async def on_trade(conn, channel, data):
+            if quote.traded:
+                return
+            # We've received a trade and might be ready to follow it
             if (
-                data.price == quote.ask
-                and quote.bid_size > (quote.ask_size * 1.8)
-                and (
-                    position.total_shares + position.pending_buy_shares
-                ) < max_shares - 100
-            ):
-                # Everything looks right, so we submit our buy at the ask
-                try:
-                    o = api.submit_order(
-                        symbol=symbol, qty='100', side='buy',
-                        type='limit', time_in_force='day',
-                        limit_price=str(quote.ask)
-                    )
-                    # Approximate an IOC order by immediately cancelling
-                    api.cancel_order(o.id)
-                    position.update_pending_buy_shares(100)
-                    position.orders_filled_amount[o.id] = 0
-                    print('Buy at', quote.ask, flush=True)
-                    quote.traded = True
-                except Exception as e:
-                    print(e)
-            elif (
-                data.price == quote.bid
-                and quote.ask_size > (quote.bid_size * 1.8)
-                and (
-                    position.total_shares - position.pending_sell_shares
-                ) >= 100
-            ):
-                # Everything looks right, so we submit our sell at the bid
-                try:
-                    o = api.submit_order(
-                        symbol=symbol, qty='100', side='sell',
-                        type='limit', time_in_force='day',
-                        limit_price=str(quote.bid)
-                    )
-                    # Approximate an IOC order by immediately cancelling
-                    api.cancel_order(o.id)
-                    position.update_pending_sell_shares(100)
-                    position.orders_filled_amount[o.id] = 0
-                    print('Sell at', quote.bid, flush=True)
-                    quote.traded = True
-                except Exception as e:
-                    print(e)
-
-    @conn.on(r'trade_updates')
-    async def on_trade_updates(conn, channel, data):
-        # We got an update on one of the orders we submitted. We need to
-        # update our position with the new information.
-        event = data.event
-        if event == 'fill':
-            if data.order['side'] == 'buy':
-                position.update_total_shares(
-                    int(data.order['filled_qty'])
+                data.timestamp <= (
+                    quote.time + pd.Timedelta(np.timedelta64(50, 'ms'))
                 )
-            else:
-                position.update_total_shares(
-                    -1 * int(data.order['filled_qty'])
-                )
-            position.remove_pending_order(
-                data.order['id'], data.order['side']
-            )
-        elif event == 'partial_fill':
-            position.update_filled_amount(
-                data.order['id'], int(data.order['filled_qty']),
-                data.order['side']
-            )
-        elif event == 'canceled' or event == 'rejected':
-            position.remove_pending_order(
-                data.order['id'], data.order['side']
-            )
+            ):
+                # The trade came too close to the quote update
+                # and may have been for the previous level
+                return
+            if data.size >= 100:
+                # The trade was large enough to follow, so we check to see if
+                # we're ready to trade. We also check to see that the
+                # bid vs ask quantities (order book imbalance) indicate
+                # a movement in that direction. We also want to be sure that
+                # we're not buying or selling more than we should.
+                if (
+                    data.price == quote.ask
+                    and quote.bid_size > (quote.ask_size * 1.8)
+                    and (
+                        position.total_shares + position.pending_buy_shares
+                    ) < max_shares - 100
+                ):
+                    # Everything looks right, so we submit our buy at the ask
+                    try:
+                        o = api.submit_order(
+                            symbol=symbol, qty='100', side='buy',
+                            type='limit', time_in_force='day',
+                            limit_price=str(quote.ask)
+                        )
+                        # Approximate an IOC order by immediately cancelling
+                        api.cancel_order(o.id)
+                        position.update_pending_buy_shares(100)
+                        position.orders_filled_amount[o.id] = 0
+                        print('Buy at', quote.ask, flush=True)
+                        quote.traded = True
+                    except Exception as e:
+                        print(e)
+                elif (
+                    data.price == quote.bid
+                    and quote.ask_size > (quote.bid_size * 1.8)
+                    and (
+                        position.total_shares - position.pending_sell_shares
+                    ) >= 100
+                ):
+                    # Everything looks right, so we submit our sell at the bid
+                    try:
+                        o = api.submit_order(
+                            symbol=symbol, qty='100', side='sell',
+                            type='limit', time_in_force='day',
+                            limit_price=str(quote.bid)
+                        )
+                        # Approximate an IOC order by immediately cancelling
+                        api.cancel_order(o.id)
+                        position.update_pending_sell_shares(100)
+                        position.orders_filled_amount[o.id] = 0
+                        print('Sell at', quote.bid, flush=True)
+                        quote.traded = True
+                    except Exception as e:
+                        print(e)
 
-    conn.run(
-        ['trade_updates', tc, qc]
-    )
+        @conn.on(r'trade_updates')
+        async def on_trade_updates(conn, channel, data):
+            # We got an update on one of the orders we submitted. We need to
+            # update our position with the new information.
+            event = data.event
+            if event == 'fill':
+                if data.order['side'] == 'buy':
+                    position.update_total_shares(
+                        int(data.order['filled_qty'])
+                    )
+                else:
+                    position.update_total_shares(
+                        -1 * int(data.order['filled_qty'])
+                    )
+                position.remove_pending_order(
+                    data.order['id'], data.order['side']
+                )
+            elif event == 'partial_fill':
+                position.update_filled_amount(
+                    data.order['id'], int(data.order['filled_qty']),
+                    data.order['side']
+                )
+            elif event == 'canceled' or event == 'rejected':
+                position.remove_pending_order(
+                    data.order['id'], data.order['side']
+                )
+
+        conn.run(
+            ['trade_updates', tc, qc]
+        )
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--symbol', type=str, default='SNAP',
-        help='Symbol you want to trade.'
+        '--symbols', nargs='*', type=str, default='SNAP',
+        help='Symbols you want to trade.'
     )
     parser.add_argument(
         '--quantity', type=int, default=500,
